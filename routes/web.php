893a78cfcb41739c28af1411a9e59f4f69710711
kssharma22 <ks.sharma0111@gmail.com>
2025-02-28 13:03:31 +0530

    done1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return success('aaaaa helllo how are you kartiknnnnnnnnnn !');
+    return success(' helllo how are you kartik g !');
     return view('welcome');
 });
