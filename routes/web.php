52413c1bd0176cf79cef9e859a97683c50acf6f5
kssharma22 <ks.sharma0111@gmail.com>
2025-02-28 13:16:04 +0530

    Fix security vulnerability by updating dependencies

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return success(' helllo how are you kartik g1111111111 !');
+    return success(' helllo how are you kartik !');
     return view('welcome');
 });
