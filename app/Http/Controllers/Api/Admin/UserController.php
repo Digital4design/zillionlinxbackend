b80a99c92d47a8f617f80a57f4a29e363db0d12e
vishwasd4d <vishwasd4d@gmail.com>
2025-03-17 17:37:39 +0530

    set time

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Api\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\Api\{LoginRequest, RegisterRequest};
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+
     public function index(Request $request)
     {
         try {
@@ -34,6 +38,35 @@ class UserController extends Controller
                 'status' => 'error',
                 'status_code' => 500,
             ], 500);
+        }
+    }
+
+    public function create(RegisterRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+
+            if (Auth::user()->role_id !== 1) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unauthorized access',
+                    'status_code' => 403,
+                ], 403);
+            }
+
+            // Create a new user
+            $data = User::create([
+                'first_name' => $validated['first_name'],
+                'last_name' => $validated['last_name'],
+                'email' => $validated['email'],
+                'country' => $request->input('country'),
+                'password' => Hash::make($validated['password']),
+                'email_verified_at' => now(),
+            ]);
+
+            return success("Registered successful");
+        } catch (\Exception $ex) {
+            return error($ex->getMessage());
         }
     }
 }
