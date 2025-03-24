87eb2fb8686256c504fd8d234bb0274a9748797b
vishwasd4d <vishwasd4d@gmail.com>
2025-03-24 10:41:22 +0530

    delete api for category in admin section

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
-    public function index(Request $request){
-        
+    public function __construct()
+    {
+        if (Auth::check() && Auth::user()->role_id !== 1) {
+            abort(response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized access',
+                'status_code' => 403,
+            ], 403));
+        }
     }
+
+    public function index(Request $request) {}
 }
