3eb74d582edcdd2ded831576ffeb511503a3338d
vishwasd4d <vishwasd4d@gmail.com>
2025-03-24 13:33:10 +0530

    bookmarks

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Bookmark;
+use App\Models\UserBookmark;
 
 class DashboardController extends Controller
 {
@@ -19,5 +22,57 @@ class DashboardController extends Controller
         }
     }
 
-    public function index(Request $request) {}
+    /*
+    * Date: 24-Mar-2025
+    *
+    * This method allows show the dashboard data:
+    * - total users, bookmark, email, password, country
+    *
+    * @param \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function index(Request $request)
+    {
+        User::where('role_id', 2);
+        try {
+            $totalUsers = User::where('role_id', 2)->count();
+            $users = User::where('role_id', 2)->orderBy('created_at', 'desc')->limit(50)->get();
+            $totalBookmark = Bookmark::count();
+            $bookmarks = UserBookmark::with('bookmark')
+                ->orderBy('position', 'asc') // Then order by position
+                ->limit(50)
+                ->get()
+                ->map(function ($userBookmark) {
+                    return [
+                        'id'             => $userBookmark->id,
+                        'bookmark_id'    => $userBookmark->bookmark_id,
+                        'user_id'        => $userBookmark->user_id,
+                        'category_id'    => $userBookmark->category_id,
+                        'sub_category_id' => $userBookmark->sub_category_id,
+                        'add_to'         => $userBookmark->add_to,
+                        'pinned'         => $userBookmark->pinned,
+                        'position'       => $userBookmark->position,
+                        'created_at'     => $userBookmark->created_at,
+                        'updated_at'     => $userBookmark->updated_at,
+                        'website_url'    => $userBookmark->bookmark->website_url ?? null,
+                        'icon_path'      => $userBookmark->bookmark->icon_path ?? null,
+                    ];
+                });
+
+
+            return response()->json([
+                'status' => 'success',
+                'total_users' => $totalUsers,
+                'user_data' => $users,
+                'totalBookmark' => $totalBookmark,
+                'bookmark_data' => $bookmarks,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went wrong. Please try again later.',
+                'status' => 'error',
+                'status_code' => 500,
+            ], 500);
+        }
+    }
 }
