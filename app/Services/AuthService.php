b67609ba1f8c0679f370a957a0015982af11ccbe
vishwasd4d <vishwasd4d@gmail.com>
2025-04-02 17:08:36 +0530

    registration

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use App\Repositories\UserRepository;
@@ -16,13 +17,18 @@ class AuthService
                 Auth::logout();
                 return error("Invalid credentials");
             }
-            
+
             $token = $user->createToken('auth_token')->plainTextToken;
-            $data = ["name"=>$user->first_name." ".$user->last_name,
-           'role' => 'user',
-           'email' => $user->email,
-        ];
-            
+            $data = [
+                "name" => $user->first_name . " " . $user->last_name,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'role' => 'user',
+                'email' => $user->email,
+                'id' => $user->id,
+                'country' => $user->country ?? null,
+            ];
+
 
             return success("Login successful", ['token' => $token, 'user' => $data]);
         }
@@ -38,25 +44,24 @@ class AuthService
             $existingUser = User::where('email', $user->email)->first();
             $data = [];
             if ($existingUser) {
-                
+
                 $token = $existingUser->createToken('auth_token')->plainTextToken;
 
                 $data['authToken'] = $token;
-                $data['user'] = $existingUser->first_name." ".$existingUser->last_name;
-                
+                $data['user'] = $existingUser->first_name . " " . $existingUser->last_name;
             } else {
                 $newUser = User::create([
                     'name' => $user->name,
-                    'email'=> $user->email,
-                    'provider'=> 'google',
+                    'email' => $user->email,
+                    'provider' => 'google',
                     'provider_id' => $user->id,
-                    
+
                     'email_verified_at' => now(),
-                    
+
                 ]);
                 $token = $newUser->createToken('auth_token')->plainTextToken;
                 $data['authToken'] = $token;
-                $data['name'] = $newUser->first_name." ".$newUser->last_name;
+                $data['name'] = $newUser->first_name . " " . $newUser->last_name;
                 // $data['display_name'] = $newUser->display_name;
             }
 
